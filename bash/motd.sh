f4e17919d3959e3eaf79d2dbd9cb57d12badd402
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2023-11-18 01:10:10 -0700

    Remove unicode chars

--- bash/motd.sh
+++ bash/motd.sh
@@ -9,14 +9,14 @@ upString=`printf "%d days, %02d:%02d:%02d" "$days" "$hours" "$mins" "$secs"`
 read load1 load5 load15 rest < /proc/loadavg
 
 echo "$(tput setaf 2)
-   .~~.   .~~.    `hostname`
-  '. \ ' ' / .'   `date +"%A, %e %B %Y, %r"`
-   .~ .~~~..~.    `uname -srmo` $(tput setaf 1)
-  : .~.'~'.~. :   $(tput setaf 2)Uptime.............: ${upString} $(tput setaf 1)
- ~ (   ) (   ) ~  $(tput setaf 2)Memory.............: `free -m | awk 'FNR == 2 {print $3}'` MB (used) / `free -m | awk 'FNR == 2 {print $2}'` MB (total) $(tput setaf 1)
+   .~~.   .~~.    `hostname`
+  '. \ ' ' / .'   `date +"%A, %e %B %Y, %r"`
+   .~ .~~~..~.    `uname -srmo` $(tput setaf 1)
+  : .~.'~'.~. :   $(tput setaf 2)Uptime.............: ${upString} $(tput setaf 1)
+ ~ (   ) (   ) ~  $(tput setaf 2)Memory.............: `free -m | awk 'FNR == 2 {print $3}'` MB (used) / `free -m | awk 'FNR == 2 {print $2}'` MB (total) $(tput setaf 1)
 ( : '~'.~.'~' : ) $(tput setaf 2)Load Averages......: ${load1}, ${load5}, ${load15} (1, 5, 15 min) $(tput setaf 1)
- ~ .~ (   ) ~. ~  $(tput setaf 2)Running Processes..: `ps ax | wc -l | tr -d " "` $(tput setaf 1)
-  (  : '~' :  )   $(tput setaf 2)IP Addresses.......: `ifconfig eth0 | grep "inet " | xargs | cut -d " " -f 2` and `wget -qO- http://ipecho.net/plain` $(tput setaf 1)
-   '~ .~~~. ~'    $(tput setaf 2)Disk Usage.........: `df -h | grep -E '^/dev/root' | awk '{ print $5 }'` on /dev/root $(tput setaf 1)
-       '~'
+ ~ .~ (   ) ~. ~  $(tput setaf 2)Running Processes..: `ps ax | wc -l | tr -d " "` $(tput setaf 1)
+  (  : '~' :  )   $(tput setaf 2)IP Addresses.......: `ifconfig enxb827ebb2454b | grep "inet " | xargs | cut -d " " -f 2` and `wget -qO- http://ipecho.net/plain` $(tput setaf 1)
+   '~ .~~~. ~'    $(tput setaf 2)Disk Usage.........: `df -h | grep -E '^/dev/root' | awk '{ print $5 }'` on /dev/root $(tput setaf 1)
+       '~'
 $(tput sgr0)"
